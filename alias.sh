bc6c18d745dd29a61af95a82434d82a1b2d9b9a2
Vittorio Iocolano <vittorio.iocolano@wrld3d.com>
2019-12-20 15:25:48 +0000

    Improve alias.sh formatting

--- alias.sh
+++ alias.sh
@@ -1,10 +1,22 @@
+alias pryderi="sirpryderi"
+
+# # # # # # # #
+# Logging
 alias tf="tail -f"
+
+# # # # # # # #
+# Docker
 alias dcmp="docker-compose"
+
+# # # # # # # #
+# System
 alias s="sudo"
 alias update="sudo apt update; sudo apt upgrade"
 alias dupdate="sudo apt update; sudo apt dist-upgrade"
+alias stutiti="shutdown -hP now"
+
+# # # # # # # #
+# Utils
 alias c="clear"
 alias n="nano"
-alias pryderi="sirpryderi"
 alias start="xdg-open"
-alias stutiti="shutdown -hP now"
