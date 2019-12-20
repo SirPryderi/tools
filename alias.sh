408d64fb985ceb78f4afbe132ec578d0e0fa7047
Vittorio Iocolano <vittorio.iocolano@wrld3d.com>
2019-12-20 15:40:19 +0000

    Add Git aliases

--- alias.sh
+++ alias.sh
@@ -22,3 +22,8 @@ alias stutiti="shutdown -hP now"
 alias c="clear"
 alias n="nano"
 alias start="xdg-open"
+
+# # # # # # # #
+# Git
+alias glg1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias glg2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
