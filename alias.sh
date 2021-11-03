b8dddf7ef17f963fa50f7270d46671685a576d35
Vittorio Iocolano <vittorio.iocolano@outlook.com>
2021-11-03 09:52:35 +0000

    Add git undo alias

--- alias.sh
+++ alias.sh
@@ -32,3 +32,4 @@ alias refresh="source $HOME/.zshrc &> /dev/null || source $HOME/.bashrc &> /dev/
 # Git
 alias glg1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias glg2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+git config --global alias.undo "reset --soft HEAD~1"
